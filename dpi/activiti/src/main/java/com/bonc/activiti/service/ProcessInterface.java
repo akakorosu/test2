916a9befb4ac6ef0a3b75dcf1da7fff793e1c5e2
刘晓冬 <liuxiaodong@liuxiaodongdeMacBook-Pro.local>
2025-09-10 09:47:50 +0800

    Initial commit for test2 project

--- dpi/activiti/src/main/java/com/bonc/activiti/service/ProcessInterface.java
+++
@@ -1,9 +0,0 @@
-package com.bonc.activiti.service;
-
-import com.bonc.activiti.entity.LocalProcessVariables;
-
-
-public interface ProcessInterface {
-
-	void updateProjectPhase(String taskPeriod, LocalProcessVariables localProcessVariables);
-}
--- dpi/dpi-webapp/src/main/webapp/pages/jsp/dpi/yuqing/dingzhi-html.jsp
+++
@@ -1,5 +0,0 @@
-<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<html>
-${html}
-</html>
